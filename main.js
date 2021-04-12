f1c7720c7491f15941da8500e181eed8496b2560
israel <israel.munoz.urias@gmail.com>
2021-04-12 16:59:16 -0600

    two

--- main.js
+++ main.js
@@ -43,6 +43,7 @@ ipcMain.on("app_version", (event) => {
 autoUpdater.on("update-available", () => {
   mainWindow.webContents.send("update_available");
 });
+
 autoUpdater.on("update-downloaded", () => {
   mainWindow.webContents.send("update_downloaded");
 });
